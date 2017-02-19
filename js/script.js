b4819b2bd0255edbcaae3fd61a9a0ee25271049f
Elena <sonaftw@mail.ru>
2017-02-19 16:07:59 +0500

    отступы и мелкие правки

--- js/script.js
+++ js/script.js
@@ -1,6 +1,5 @@
 var indexFormOpen=document.querySelector(".btn-brown");
 var indexFormStatus=document.querySelector(".search-hotel-form");
-var map=document
 indexFormOpen.addEventListener( "click", function(event) {
   event.preventDefault();
   indexFormStatus.classList.toggle('index-form-show');
